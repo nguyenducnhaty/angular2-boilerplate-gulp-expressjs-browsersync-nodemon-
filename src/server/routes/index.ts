ce263b5feb483b7b12374ea8c4e332c8538e51ff
Nirmal Kumar <vnknirmalkumar@gmail.com>
2016-04-10 17:59:09 +0800

    title changed forAngular2 Boiler Plate

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,7 +2,7 @@ import express = require("express");
 let router = express.Router();
 
 let renderIndex = (req: express.Request, res: express.Response) => {
-    res.render("index", { title: "Smart Quiz" });
+    res.render("index", { title: "Angular2 Boiler Plate" });
 }
 
 /* GET home page.*/
